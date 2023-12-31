83a879e5a7c3c18acc1e6eabba54deaa1b28e53f
ramer-dev <gml4009@gmail.com>
2023-12-31 23:11:31 +0900

    fix : 텍스트 수정

--- src/Modal/Share.jsx
+++ src/Modal/Share.jsx
@@ -15,7 +15,16 @@ function Share({ isOpen, setIsOpen }) {
 
     const makeURL = () => {
         if (inputRef.current) {
-            let url = window.location.host + '?n=' + encodeToBase64(inputRef.current.value);
+            // 아무것도 입력 안했을 경우
+            if(inputRef.current.value === '') {
+                window.navigator.clipboard.writeText(window.location.protocol + "//" + window.location.host).then(() => {
+                    window.alert('링크 복사가 완료되었습니다.')
+                })
+                return;
+            }
+
+            //정상 입력 경우 (12자 이상 자름)
+            let url = window.location.protocol + "//" + window.location.host + '?n=' + encodeToBase64(inputRef.current.value.substring(0,12));
 
             window.navigator.clipboard.writeText(url).then(() => {
                 window.alert('링크 복사가 완료되었습니다.')
@@ -33,7 +42,7 @@ function Share({ isOpen, setIsOpen }) {
                 <div className='modal'>
                     <h4>공유하고싶은 분의 이름을 적어주세요.</h4>
                     <div className='modal-div'>
-                        <input type='text' placeholder='이름' ref={inputRef}></input>
+                        <input type='text' placeholder='이름(12자 이하)' maxLength={12} ref={inputRef}></input>
                         <button type='button' className='modal-confirm' onClick={makeURL}>링크 복사</button>
                     </div>
                 </div>
