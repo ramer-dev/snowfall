0ea6233ef2f8c6159fab3ec98ec990c6a1fda9ec
ramer-dev <gml4009@gmail.com>
2023-12-31 23:32:16 +0900

    feat 토스트 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Window from './Canvas/Window';
+import Toast from './Toast/Toast';
 
 function App() {
+
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsOpen(true)
+  }, [])
   return (
     <div className="App">
       <div className='container'>
         <div className='wrapper'>
           {/* <img className='window' src='#' alt='window BG'/> */}
           <Window />
+          <Toast isOpen={isOpen} setIsOpen={setIsOpen} content={'터치/클릭하여 쌓인 눈을 닦을 수 있습니다.'} time={3000}/>
         </div>
 
       </div>
